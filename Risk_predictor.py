32dd94f97dc93e1ca0dbdf2d7efbfd5b8a7379e4
Manasa <manasars@uw.edu>
2024-02-21 12:26:19 -0800

    Added color coding to the predicted risk %

--- Risk_predictor.py
+++ Risk_predictor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import joblib  
 
-# ### TO BE DELETED AFTER TETSING ##
+# # ### TO BE DELETED AFTER TETSING ##
 # test_data = {
 #     'DPQ020': [3], 
 #     'DPQ050': [3], 
@@ -24,11 +24,20 @@ def risk_predict(Inputlist):
     # Predict risk using the test data
     new_prediction = loaded_model.predict_proba(pd.DataFrame(test_data))[:, 1]  # Probability of class 1 (obese)
     
-    Obesityrisk = round(new_prediction[0] * 100, 1)
-    print(f'Predicted Obesity Risk: {Obesityrisk} %')
-    return Obesityrisk
-   
+    obesityrisk = round(new_prediction[0] * 100, 1)
+    
+    if 0 <= obesityrisk <= 25:
+        color = 'Blue'  # Low risk
+    elif 25 < obesityrisk <= 50:
+        color = 'Green'  # Moderate risk
+    elif 50 < obesityrisk <= 75:
+        color = 'Yellow'  # High risk
+    else:
+        color = 'Red'  # Very high risk
+    
+    return obesityrisk, color
 
-risk_predict(test_data)
+# result = risk_predict(test_data)
+# print(f'Predicted Obesity Risk: {result[0]} %, {result[1]}')
 
 
