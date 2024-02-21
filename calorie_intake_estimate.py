e6b6db5da271194eef7aab830afe1d0910d9c96d
Manasa <manasars@uw.edu>
2024-02-21 12:14:29 -0800

    Updated for Height(cm) weight metric(Kg) units

--- calorie_intake_estimate.py
+++ calorie_intake_estimate.py
@@ -1,5 +1,5 @@
 
-def calculate_bmr(weight, height, age, sex):
+def calculate_bmr(weight_kg, height_cm, age, sex):
     # Calculate Basal Metabolic Rate (BMR) based on weight, height, age, and sex.
     # Args:
     # weight (float): Weight in kilograms.
@@ -10,17 +10,17 @@ def calculate_bmr(weight, height, age, sex):
     # float: BMR value.
     
     if sex == 1:
-        bmr = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age)
+        bmr = 66 + (6.3 * weight_kg) + (12.9 * height_cm) - (6.8 * age)
     
     elif sex == 0:
-        bmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)
+        bmr = 655 + (4.3 * weight_kg) + (4.7 * height_cm) - (4.7 * age)
     
     else:
         raise ValueError("Invalid value for sex")
     
     return bmr
 
-def calculate_calorie_intake(weight, height, age, sex, activity_level):
+def calculate_calorie_intake(weight_kg, height_cm, age, sex, activity_level):
     # Calculate total calorie intake based on BMR and activity level.
     # Args:
     # weight (float): Weight in kilograms.
@@ -31,7 +31,7 @@ def calculate_calorie_intake(weight, height, age, sex, activity_level):
     # Returns:
     # float: Total calorie intake.
     try:
-        bmr = calculate_bmr(weight, height, age, sex)
+        bmr = calculate_bmr(weight_kg, height_cm, age, sex)
     
     except ValueError as ve:
         raise ValueError(ve)
@@ -50,17 +50,10 @@ def calculate_calorie_intake(weight, height, age, sex, activity_level):
     calorie_intake = bmr * activity_multiplier[activity_level]
     return calorie_intake
 
-
-# # Example usage : To be removed in final version of the code. Uncomment for test run
-  
-# weight = float(input("Enter weight in kg: "))
-# height = float(input("Enter height in cm: "))
-# age = int(input("Enter age in years: "))
-# sex = input("Enter sex (male/female): ")
-# activity_level = input("Enter activity level: ")
-    
-# # This call must be made from streamlit & output has to be rendered appropriately. 
-# calorie_intake = calculate_calorie_intake (float(56), float(150), int(30), 0, int(2))
-# print("Estimated calorie intake:", calorie_intake, "kcal")
-
+'''
+# Example usage : To be removed in final version of the code. Uncomment for test run    
+# This call must be made from streamlit & output has to be rendered appropriately. 
+calorie_intake = calculate_calorie_intake (float(56), float(150), int(30), 0, int(2))
+print("Estimated calorie intake:", calorie_intake, "kcal")
+'''
 
