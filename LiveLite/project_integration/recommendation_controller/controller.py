2363fde9ca2b0850c3e3b4158ea4fdd8d6149daf
Parvati <parvatijay2901@gmail.com>
2024-02-24 19:35:49 -0800

    Streamlit v0.4 - Integrate Diet recommendation - part1

--- LiveLite/project_integration/recommendation_controller/controller.py
+++ LiveLite/project_integration/recommendation_controller/controller.py
@@ -11,6 +11,14 @@ def controller(choice):
     user_inputs = st.session_state['user_inputs']
     mapped_user_inputs = project_integration.user_input_mapping(user_inputs)
     
+    if choice == "estimate_calorie_intake":
+        calorie_intake = recommendation_tool.calculate_calorie_intake(mapped_user_inputs['internal_weight'],
+                                                                    mapped_user_inputs['internal_height'],
+                                                                    mapped_user_inputs['internal_age'],
+                                                                    mapped_user_inputs['internal_sex'],
+                                                                    mapped_user_inputs['internal_activity_level'])
+        return calorie_intake
+    
     if choice == "risk_score":
         is_obese = recommendation_tool.is_obese(mapped_user_inputs['internal_height'], mapped_user_inputs['internal_weight'])
         if is_obese:
@@ -20,6 +28,11 @@ def controller(choice):
             model = "./recommendation_tool/risk_assessment/trained_models/obesity_risk_model.joblib"
             risk_score = recommendation_tool.risk_predict(risk_predict_input, model)
         return risk_score
+    
+    if choice == "diet_recommender":
+        macro_nutrients_df = recommendation_tool.macro_nutrients_data(mapped_user_inputs['internal_age'], mapped_user_inputs['internal_sex'], st.session_state['calorie_intake'])
+        micro_nutrients_df = recommendation_tool.micro_nutrients(mapped_user_inputs['internal_age'], mapped_user_inputs['internal_sex'])
+        return [macro_nutrients_df, micro_nutrients_df]
     
     if choice == "physical_activity_recommender":
         filename = "./data/input_files/calories_burned_30_minutes.csv"
--- LiveLite/streamlit_app/pages/3_risk_insights.py
+++ LiveLite/streamlit_app/pages/3_risk_insights.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import project_integration
+from streamlit_extras.stylable_container import stylable_container
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
@@ -33,11 +34,31 @@ with col2:
     view_recommendations = st.button("View Personalized Recommendations 🌿🏃🏼", use_container_width=True)
     project_integration.add_blank_lines(2)
 if view_recommendations:
+    calorie_intake = project_integration.controller("estimate_calorie_intake")
+    st.markdown(f"""<div style="text-align: center;"><h3>Estimated Daily Calorie Intake: {calorie_intake:.1f}Kcal</h3></div>""", unsafe_allow_html=True)
+    st.session_state['calorie_intake'] = calorie_intake
     col1, _, col3 = st.columns([1,0.2,1])
     with col1:
         ## TO BE MODIFIED
         st.markdown("""<div style="text-align: center;"><h3 style='color:gold;'>Diet Recommendation</h3></div>""", unsafe_allow_html=True)
-        # 1. Show the macro nutrient table
+        project_integration.add_blank_lines()
+        st.markdown("""<div style ="text-align: center;"><p style='color:#99fadc;'>Ensure you meet the recommended daily intake levels for these macro nutrients for optimal health and performance!</p></div>""", unsafe_allow_html=True)
+        diet_recommendation = project_integration.controller("diet_recommender")
+        _, col112, _ = st.columns([1,25,1])
+        with col112:
+            project_integration.add_blank_lines()
+            st.dataframe(diet_recommendation[0], hide_index=True)
+            project_integration.add_blank_lines(2)
+        st.markdown("""<div style ="text-align: center;"><p style='color:#99fadc;'>Ensure adequate intake of these micro nutrients to support overall health and vitality!</p></div>""", unsafe_allow_html=True)
+        _, col122, _ = st.columns([1,2,1])
+        with col122:
+            project_integration.add_blank_lines()
+            st.dataframe(diet_recommendation[1], hide_index=True)
+            project_integration.add_blank_lines()
+            #with stylable_container("button", css_styles="""button {background-color: #f2f2f2; color: black;}"""):
+            if st.button("View food recommendations 🥦", use_container_width=True):
+                    pass
+                
         # 2. "View more recommendations" button
         # 3. provide a dropdown with unique food categories
             # 3.1 Filter to only have that dataframe
@@ -46,7 +67,7 @@ if view_recommendations:
     with col3:
         st.markdown("""<div style="text-align: center;"><h3 style='color:gold;'>Physical Activity Recommendation</h3></div>""", unsafe_allow_html=True)
         project_integration.add_blank_lines()
-        st.markdown("""<div style ="text-align: center;">Get fit and live a healthier life with these activities!</div>""", unsafe_allow_html=True)
+        st.markdown("""<div style ="text-align: center;"><p style='color:#99fadc;'>Get fit and live a healthier life with these activities!</p></div>""", unsafe_allow_html=True)
         _, col32, _ = st.columns([1,25,1])
         with col32:
             project_integration.add_blank_lines()
