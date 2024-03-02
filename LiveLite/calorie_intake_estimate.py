12c89be10282600ce9c440c6c3143ec4ceaa1f73
Manasa <manasars@uw.edu>
2024-03-02 15:33:35 -0800

    Updated code for PEP8 compliant

--- LiveLite/calorie_intake_estimate.py
+++ LiveLite/calorie_intake_estimate.py
@@ -1,22 +1,23 @@
-""" This module computes the bmr values & utilizes this bmr value to
-    compute estimation of daily calorie intake for the user
+""" 
+This module computes estimation of daily calorie intake for the user.
+
+Functions:
+- calculate_bmr()
+- calculate_calorie_intake()
 """
+
 def calculate_bmr(weight_kg, height_cm, age, sex):
     """ Calculate BMR based on weight, height, age, and sex.
-
     Args:
         weight_kg (float): Weight in kilograms
         height_cm (float): Height in centimeters
         age (int): Age in years.
         sex (boolean): Gender of the person.'0 - female' or '1 - male'
-
     Raises:
         ValueError: Invalid gender value
-
     Returns:
         float: BMR value
     """
-
     if sex == 1:
         bmr = 66 + (6.3 * weight_kg) + (12.9 * height_cm) - (6.8 * age)
     elif sex == 0:
@@ -28,14 +29,12 @@ def calculate_bmr(weight_kg, height_cm, age, sex):
 def calculate_calorie_intake(weight_kg, height_cm, age, sex, activity_level):
     """ This function calculates the estimated calorie intake based on
         the user input data and bmr calculated.
-
     Args:
         weight_kg (float): Weight in kilograms
         height_cm (float): Height in centimeters
         age (int): Age in years.
         sex (boolean): Gender of the person. Either '0 - female' or '1 - male'
         activity_level (int): Activity level ranging from 1 - 5
-
     Raises:
         ValueError: Inavlid bmr
         ValueError: Invalid activity level 
@@ -43,12 +42,12 @@ def calculate_calorie_intake(weight_kg, height_cm, age, sex, activity_level):
     Returns:
         float: calorie intake in kcal
     """
-
     try:
         bmr = calculate_bmr(weight_kg, height_cm, age, sex)
     except ValueError as ve:
         raise ValueError(ve) from ve
 
+    # Different multiplier for each activity level.
     activity_multiplier = {
         1 : 1.2,
         2 : 1.375,
@@ -56,7 +55,6 @@ def calculate_calorie_intake(weight_kg, height_cm, age, sex, activity_level):
         4 : 1.725,
         5 : 1.9
     }
-
     if activity_level not in activity_multiplier:
         raise ValueError("Invalid value for activity level")
 
